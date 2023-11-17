95a67e550597d249c5d4f48c8243d46aa9670d1e
ferdous_7272 <ferdous.frd81@gmail.com>
2023-11-18 00:11:34 +0200

    Made app skeleton with html and css

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputEl = document.getElementById("input-field")
+const addBtnEl = document.getElementById("add-btn")
+
+
+addBtnEl.addEventListener("click", function(){
+    let inputValue = inputEl.value
+    console.log(inputValue)
+})
