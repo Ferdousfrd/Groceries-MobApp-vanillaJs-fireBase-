150d841c1c20c6d9462baba8bbb1c8cb152545a2
ferdous_7272 <ferdous.frd81@gmail.com>
2023-11-18 21:05:06 +0200

    added database from firebase

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"  //importing 'initializeApp' function
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"  //importing 'getDatabse,ref and push' function
+
+
+const appSettings = {
+    databaseURL : "https://groceriesapp-3b819-default-rtdb.europe-west1.firebasedatabase.app/"  //our database in firebase
+}
+
+const app = initializeApp(appSettings)              //initializing firebase app so that we can use firebase database
+const database = getDatabase(app)                   //connecting our database 
+const groceriesInBag = ref(database, "groceris")    //adding reference to the database and naming items
+
 const inputEl = document.getElementById("input-field")
 const addBtnEl = document.getElementById("add-btn")
 
 
 addBtnEl.addEventListener("click", function(){
     let inputValue = inputEl.value
-    console.log(inputValue)
+    push(groceriesInBag, inputValue)                //pushing the values in our refrenced database and items
+    console.log(`${inputValue} added to the databse`)
 })
