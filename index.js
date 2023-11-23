883b479c157e845e41cf141c5b31e75ec262f178
ferdous_7272 <ferdous.frd81@gmail.com>
2023-11-23 20:57:09 +0200

    Finalized protype 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"  //importing 'initializeApp' function
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"  //importing 'getDatabse,ref and push' function
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"  //importing 'initializeApp' function for firebase
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"  //importing necessary methods
 
 
 const appSettings = {
@@ -22,15 +22,20 @@ addBtnEl.addEventListener("click", function(){
     clearInputSection()
 })
 
-onValue(groceriesInBag, function(snapshot){
-    let databaseListArray = (Object.entries(snapshot.val()))
-    clearShoppingListEl()
-    for(let i = 0;i<databaseListArray.length;i++){
-        let currentItem = databaseListArray[i]
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
+onValue(groceriesInBag, function(snapshot){                             //snapshot contains data from the database
+    if(snapshot.exists()){                                              //checking if there is any item in the database or if it is null
+        let databaseListArray = (Object.entries(snapshot.val()))        //bringing objects both id and values
+        clearShoppingListEl()
+        for(let i = 0;i<databaseListArray.length;i++){
+            let currentItem = databaseListArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
 
-        addingItemInList(currentItemID)
+            addingItemInList(currentItem)
+        }    
+    }
+    else{
+        shoppingListEl.innerHTML = "No Items Here.... "
     }
 })
 
@@ -38,8 +43,18 @@ onValue(groceriesInBag, function(snapshot){
 function clearInputSection(){
     inputEl.value = ""
 }
-function addingItemInList(itemValue){
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+function addingItemInList(item){
+    let newEl = document.createElement("li")                //we create a newEl element which is a listItem
+    let itemID = item[0]          
+    let itemValue = item[1]         
+ 
+    newEl.textContent = itemValue                         //we put text content inside
+    shoppingListEl.append(newEl)                            //we append() that in shoppingListEl or in our ul
+
+    newEl.addEventListener("dblclick", function(){
+        let exactLocationOfItemID = ref(database, `groceris/${itemID}`)     
+        remove(exactLocationOfItemID)
+    })
 
 }
 function clearShoppingListEl(){
